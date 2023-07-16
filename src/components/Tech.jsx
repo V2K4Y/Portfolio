0ba176c0da4d7d43d42ea624f9fdd7d39ec56366
Vivek <=>
2023-07-16 16:39:27 +0530

    tech,project done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
+import { motion } from "framer-motion"
+import { styles } from "../style"
+import { fadeIn, textVariant } from "../utils/motion"
 
-function Tech() {
+
+const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+      <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Skillset i have</p>
+          <h2 className={styles.sectionHeadText}>Technology.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        HTML, CSS, JS, building small and medium web applications with React, Node and Express, custom plugins, features, animations, and coding interactive layouts. I have also full-stack developer experience
+      </motion.p>
+
+      <div className="flex flex-row flex-wrap justify-center pt-[60px] gap-10">
+        {technologies.map((technology) => (
+          <div className="w-28 h-28 mt-[30px]" key={technology.name}>
+            <BallCanvas icon = {technology.icon} />
+          </div>
+        ))}
+        
+      </div>
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
